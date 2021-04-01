5579cbfa8484dde13d9a3cb1e0a01ff891e4e2a0
Alex Gavrilov <alex@talentmesh.com>
2021-04-01 14:45:08 +0000

    small fix

--- src/Forms/EditFormParent.tsx
+++ src/Forms/EditFormParent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { RouteComponentProps, useHistory } from 'react-router-dom';
 import EditForm, { EditFormProps } from './EditForm';
 
--- src/Routing/ClaimNames.tsx
+++
@@ -1,3 +0,0 @@
-export default class ClaimNames {
-    static readonly Role = 'https://create.talentmesh-dev.com/api/roles';
-}
